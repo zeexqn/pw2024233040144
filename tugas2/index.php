61cb7174490897860995e85b5e6cfee463f09f1c
Zeina <145121493+zeexqn@users.noreply.github.com>
2024-02-19 23:46:56 +0700

    Add files via upload

---
+++ tugas2/index.php
@@ -0,0 +1,19 @@
+<?php 
+$npm = 44;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2</title>
+</head>
+<body>
+    <p>Aku adalah angka <b><?php echo $npm; ?></b></p>
+    <p>Jika aku kali 5, maka aku sekarang menjadi <b><?php echo $npm * 5 ?></b></p>
+    <p>Jika aku bagi 2, maka aku sekarang menjadi <b><?php echo $npm / 2 ?></b></p>
+    <p>Jika aku tambah 75, maka aku sekarang menjadi <b><?php echo $npm + 75 ?></b></p>
+    <p>Jika aku kurang 20, maka aku sekarang menjadi <b><?php echo $npm - 20 ?></b></p>
+</body>
+</html>
---
+++ tugas3/index.html
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="box">
+        <div class="box-child">1</div>
+    </div>
+    <div class="box">
+        <div class="box-child">2</div>
+        <div class="box-child">2</div>
+    </div>
+    <div class="box">
+        <div class="box-child">3</div>
+        <div class="box-child">3</div>
+        <div class="box-child">3</div>
+    </div>
+</body>
+</html>
