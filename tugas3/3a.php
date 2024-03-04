d73b0774bd8ae6a82e4396e038e188f8795242d5
Zeina <145121493+zeexqn@users.noreply.github.com>
2024-03-05 01:11:02 +0700

    Add files via upload

---
+++ tugas3/3a.php
@@ -0,0 +1,32 @@
+<?php 
+
+echo "<h4>Menghitung Luas Lingkaran</h4>";
+
+function hitungLuaslingkaran($r) {
+    $luas = M_PI * $r * $r;
+    return $luas;
+}
+
+$r = 10;
+$luasLingkaran = hitungLuaslingkaran($r);
+
+echo "<p>Jari-jari = $r cm</p>";
+echo "<p>Luas lingkaran = $luasLingkaran cm<sup>2</sup></p>";
+
+echo "<hr>";
+
+echo "<h4>Menghitung Keliling Lingkaran</h4>";
+
+function hitungKelilingLingkaran($r) {
+    $keliling = 2 * M_PI * $r;
+    return $keliling;
+}
+
+$r = 20;
+$kelilingLingkaran = hitungKelilingLingkaran($r);
+
+echo "<p>Jari-jari = $r cm</p>";
+echo "<p>Keliling lingkaran = $kelilingLingkaran cm</p>";
+
+
+?>
